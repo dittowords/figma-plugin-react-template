590e53ea5f8ed62d12abaff037e31b794001334a
XavierAgostini <xavier.agostini@mail.mcgill.ca>
2021-05-10 09:04:54 -0700

    get working

--- src/TextItem.tsx
+++ src/TextItem.tsx
@@ -1,20 +1,27 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 const TextItem = ({ node, handleUpdateText }) => {
   const [isEditing, setIsEditing] = useState(false)
   const [text, setText] = useState(node.text)
+  const inputRef: any = useRef()
+
+  useEffect(() => {
+    if (isEditing && inputRef.current) {
+      inputRef.current.focus()
+    }
+  }, [isEditing])
 
   return (
     <div className="textItem">
       {!isEditing ? (
         <div 
           onClick={() => setIsEditing(true)}
-         
         >
           {node.text}
           </div>
       ): (
         <input
+          ref={inputRef}
           value={text} 
           onChange={e => setText(e.target.value)}
           onBlur={() => setIsEditing(false)}
